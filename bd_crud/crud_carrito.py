9ace721483d1d5c05b563a4f4b5d1facd47a3917
veraniegoscorp <vicente.valenzuela15@inacapmail.cl>
2025-12-12 12:05:56 -0300

    sysdate reeemplzces

--- bd_crud/crud_carrito.py
+++ bd_crud/crud_carrito.py
@@ -13,12 +13,11 @@ def insertar_carrito(id_cliente, fecha_creacion,subtotal,descuento_aplicado,tota
             INSERT INTO CARRITO (
                 ID_CARRITO, ID_CLIENTE, FECHA_CREACION, SUBTOTAL, DESCUENTO_APLICADO, TOTAL
             ) VALUES (
-                :id_carrito, :id_cliente, :fecha_creacion, :subtotal, :descuento_aplicado, :total
+                :id_carrito, :id_cliente, SYSDATE, :subtotal, :descuento_aplicado, :total
             )
         """, {
             "id_carrito": id_carrito,
             "id_cliente": id_cliente,
-            "fecha_creacion": fecha_creacion,
             "subtotal": subtotal,
             "descuento_aplicado": descuento_aplicado,
             "total": total
--- main.py
+++ main.py
@@ -23,7 +23,16 @@ def crear_nuevo_cliente():
         print(f"Error al crear cliente: {e}")
 
 
-
+def crear_nuevo_carrito(nuevo_cliente):
+    try:
+        # insertar_carrito debe devolver el ID del carrito creado
+        id_carrito = insertar_carrito(nuevo_cliente.id_cliente)
+        
+        print(f"Carrito creado con ID: {id_carrito} para el cliente {nuevo_cliente.nombre} (ID: {nuevo_cliente.id_cliente})")
+        return id_carrito  # opcional, por si lo necesitas después
+    except Exception as e:
+        print(f"Error al crear carrito: {e}")
+        return None
 
 
 
